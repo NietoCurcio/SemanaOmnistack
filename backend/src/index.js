8bbd898514d2e800f0967c221ba61d1e389b03b0
Felipe Curcio <felipe_nieto010@hotmail.com>
2020-03-29 03:40:39 -0300

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,38 @@
+const express =require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(routes);
+//recurso vem dentro da barra, é a rota
+/**
+ * get: buscar/listar informação dentro do backend
+ * post: criar informação no backend
+ * put: alterar informação no backend
+ * delete: deletar informação
+ */
+
+ /**
+  * tipos de parametros:
+  * 
+  * Query: parametros nomeados eviados na rota após o "?", (Filtrar, paginação)
+  * Route: parametros utilizados para identificar recursos
+  * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+  */
+
+/**
+  * SQL: MySQL, SQLite, Oracle, Ms sql server
+  * NoSQL: MongoDB e CouchDB
+  */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ */
+  //para o back-end continuar sendo executado, precisamos do npm start
+
+app.listen(3333);
